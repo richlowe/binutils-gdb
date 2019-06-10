29c08c294f213bfdad45c36541d799eee6409464
Richard Lowe <richlowe@richlowe.net>
2019-06-10 02:00:42 +0000

    Add target descriptions for x86 Solaris architectures
    [taken from solaris-userland/components/gdb]

---
+++ gdb/features/i386/32bit-solaris.c
@@ -0,0 +1,16 @@
+/* THIS FILE IS GENERATED.  -*- buffer-read-only: t -*- vi:set ro:
+  Original: 32bit-solaris.xml */
+
+#include "defs.h"
+#include "osabi.h"
+#include "target-descriptions.h"
+
+struct target_desc *tdesc_32bit_solaris;
+static void
+initialize_tdesc_32bit_solaris (void)
+{
+  struct target_desc *result = allocate_target_description ();
+  struct tdesc_feature *feature;
+
+  tdesc_32bit_solaris = result;
+}
